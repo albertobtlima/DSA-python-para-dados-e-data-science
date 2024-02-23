39aecc2517d81044654d80b31ad8bca3d70f7dde
Alberto Lima <alberto.btlima@outlook.com>
2024-02-23 15:39:02 -0300

    Create cap04 ex02

---
+++ Cap-04/cap04-ex02.py
@@ -0,0 +1,29 @@
+# Capítulo 4 - Variáveis e Operadores
+
+var_teste = 1              # Atribuindo o valor 1 a variável var_teste
+print(var_teste)           # Imprimindo o valor da variável
+
+var_teste = 2              # Alterar o valor da variável var_teste de 1 para 2
+print(var_teste)           # Imprimindo o valor da variável
+
+print(type(var_teste))     # Verificar o tipo da variável
+
+var_teste = 9.5            # Alterar o valor da variável var_teste de 2 para 9.5
+print(type(var_teste))     # Verificar o tipo da variável
+
+x = 4                      # Atribuindo o valor 4 a variável x
+print(x)                   # Imprimindo o valor da variável
+
+print("-=" * 15)   # ------------------------------------------------------------
+
+# Declaração Múltipla
+
+pessoa1, pessoa2, pessoa3 = "Bob", "Maria", "Ana"
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
+
+fruta1 = fruta2 = fruta3 = "Melancia"
+print(fruta1)
+print(fruta2)
+print(fruta3)
