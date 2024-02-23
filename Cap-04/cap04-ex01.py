54d1b7e04ee82c8fec8653bc9105963b83231204
Alberto Lima <alberto.btlima@outlook.com>
2024-02-23 15:15:36 -0300

    Create cap04 ex01

---
+++ Cap-04/cap04-ex01.py
@@ -0,0 +1,61 @@
+# Capítulo 4 - Números e Operações Matemáticas
+
+# ------------------------------------------------
+# Operações Aritméticas
+
+print(4 + 4)       # Soma
+print(4 - 3)       # Subtração
+print(3 * 3)       # Multiplicação
+print(3 / 2)       # Divisão
+print(4 ** 2)      # Potência
+print(10 % 3)      # Módulo
+
+print("-=" * 15)   # ------------------------------------------------
+
+# Função Type
+
+print(type(5))
+print(type(5.0))
+a = 'Estou estudando na Data Science Academy'
+print(type(a))
+
+print("-=" * 15)   # ------------------------------------------------
+
+# Operações com Números Float
+
+print(3.1 + 6.4)
+print(4 + 4.0)
+print(4 + 4)
+print(4 / 2)       # Resultado é um número float - divisão real
+print(4 // 2)      # Resultado é um número float - divisão inteira
+print(4 / 3.0)
+print(4 // 3.0)
+
+print("-=" * 15)   # ------------------------------------------------
+
+# Conversão
+
+print(float(9))
+print(int(6.0))
+print(int(6.5))
+print(int(6.8))
+
+print("-=" * 15)   # ------------------------------------------------
+
+# Hexadecimal e Binário
+
+print(hex(394))
+print(hex(217))
+print(bin(286))
+print(bin(390))
+
+print("-=" * 15)   # ------------------------------------------------
+
+# Funções abs, round e pow
+
+print(abs(-8))                    # Retorna o valor absoluto
+print(abs(8))                     # Retorna o valor absoluto
+print(round(3.14151922, 2))       # Retorna o valor com duas casas decimais
+print(round(3.14151922, 1))       # Retorna o valor com uma casa decimal
+print(pow(4, 2))                  # Potência
+print(pow(5, 3))                  # Potência
