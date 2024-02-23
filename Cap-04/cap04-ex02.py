24828abf2f183ea1247772449394fdda8d1f4829
Alberto Lima <alberto.btlima@outlook.com>
2024-02-23 16:00:18 -0300

    Update cap04 ex02

--- Cap-04/cap04-ex02.py
+++ Cap-04/cap04-ex02.py
@@ -27,3 +27,54 @@ fruta1 = fruta2 = fruta3 = "Melancia"
 print(fruta1)
 print(fruta2)
 print(fruta3)
+
+print("-=" * 15)   # ------------------------------------------------------------
+
+# Pode-se usar letras, números e underline em nome de variável (mas não se pode começar com números).
+
+x1 = 50
+print(x1)
+
+#  Ira dar erro  ->  x1 = 50
+
+"""
+    Não se pode usar palavras reservadas como nome de variável
+    Ex: break = 1
+"""
+
+print("-=" * 15)   # ------------------------------------------------------------
+
+# Variáveis Atribuídas a Outras Variáveis e Ordem dos Operadores
+
+largura = 2
+altura = 4
+area = largura * altura
+print(area)
+
+perimetro = 2 * largura + 2 * altura
+print(perimetro)
+
+perimetro = 2 * (largura + 2) * altura      # A ordem dos operadores é a mesma seguida na Matemática
+print(perimetro)
+
+print("-=" * 15)   # ------------------------------------------------------------
+
+# Operações com Variáveis
+
+idade1 = 25
+idade2 = 35
+
+print(idade1 + idade2)
+print(idade2 - idade1)
+print(idade2 * idade1)
+print(idade2 / idade1)
+print(idade2 % idade1)
+
+print("-=" * 15)   # ------------------------------------------------------------
+
+# Concatenação de Variáveis
+
+nome = "Bob"
+sobrenome = "Marley"
+fullName = nome + " " + sobrenome
+print(fullName)
