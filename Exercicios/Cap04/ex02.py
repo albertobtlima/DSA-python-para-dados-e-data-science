42608e81e08e1916cd2907ae18e9ac5e8e13a7f3
Alberto Lima <alberto.btlima@outlook.com>
2024-02-24 13:56:12 -0300

    Update exercicios

--- Exercicios/Cap04/ex02.py
+++ Exercicios/Cap04/ex02.py
@@ -1,2 +1,5 @@
 objetos = ['mesa', 'cadeira', 'caderno', 'caneta', 'borracha']
 print(objetos)
+
+for i in objetos:
+    print(i)
---
+++ Exercicios/Cap04/ex03.py
@@ -0,0 +1,6 @@
+texto1 = 'Gato'
+texto2 = 'Doce'
+
+textoCompleto = texto1 + " " + texto2
+
+print(textoCompleto)
