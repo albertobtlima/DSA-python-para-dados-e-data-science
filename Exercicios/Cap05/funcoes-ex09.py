e4e688dc8cc2ebcc594c638f046813dd44ac1a03
Alberto Lima <alberto.btlima@outlook.com>
2024-03-04 12:13:42 -0300

    Update exercicios

---
+++ Exercicios/Cap05/funcoes-ex09.py
@@ -0,0 +1,5 @@
+palavras = ["maça", "coiote", "banana", "terreno", "Python"]
+
+resultado = [x for x in palavras if "a" in x]
+
+print(resultado)
---
+++ Exercicios/Cap05/funcoes-ex10.py
@@ -0,0 +1,1 @@
+print([x for x in range(1, 11) if x < 5])
